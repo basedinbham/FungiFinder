bd451a1c9fb36436b9b430c5c2a0d4d69b9d17bb
Kyle Warren <78629937+basedinbham@users.noreply.github.com>
2021-11-12 18:45:06 -0800

    font issue

--- FungiFinderFinal/Controllers/ViewControllers/Observations/ObservationDetailViewController.swift
+++ FungiFinderFinal/Controllers/ViewControllers/Observations/ObservationDetailViewController.swift
@@ -50,7 +50,7 @@ class ObservationDetailViewController: UIViewController, UITextViewDelegate, UNU
     @IBAction func saveButtonTapped(_ sender: Any) {
         
         guard let name = nameTextField.text, !name.isEmpty,
-              let type = typeButton.currentTitle, type != "Select Type",
+              let type = typeButton.currentTitle, type != "Mushroom Type",
               let notes = notesTextField.text, !notes.isEmpty else { presentRequiredTextAlert(); return }
         let latitude = saveLat
         let longitude = saveLong
@@ -346,20 +346,8 @@ extension ObservationDetailViewController: UIImagePickerControllerDelegate & UIN
 
 extension ObservationDetailViewController: MushroomTypeDelegate {
     func didSelectMushroom(name: String) {
-//        typeButton.setTitle(name, for: .normal)
-        let style = NSMutableParagraphStyle()
-        style.alignment = NSTextAlignment.left
-        style.lineBreakMode = NSLineBreakMode.byWordWrapping
-        
-        let titleAttributes: [NSAttributedString.Key : Any] = [
-            NSAttributedString.Key.foregroundColor: UIColor.label,
-            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 14),
-//            preferredFont(forTextStyle: UIFont.TextStyle.title1),
-            NSAttributedString.Key.paragraphStyle : style
-        ]
-        
-        let attributedString = NSMutableAttributedString(string: name, attributes: titleAttributes)
-        typeButton.setAttributedTitle(attributedString, for: .normal)
+        // If both title, & attributed title are set swift will prefer the attributed
+        typeButton.setTitle(name, for: .normal)
         observation?.type = name
     }
 } // End of Extension
--- FungiFinderFinal/Helpers+Extensions/TypeButton.swift
+++ FungiFinderFinal/Helpers+Extensions/TypeButton.swift
@@ -9,28 +9,10 @@ import UIKit
 
 class typeButton: UIButton {
     
-    required init?(title: String) {
-        super.init(frame: CGRect.zero)
-//    override init(frame: CGRect) {
-//        super.init(frame: frame)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         
-        let style = NSMutableParagraphStyle()
-        style.alignment = NSTextAlignment.left
-        style.lineBreakMode = NSLineBreakMode.byWordWrapping
-        
-        let titleAttributes: [NSAttributedString.Key : Any] = [
-            NSAttributedString.Key.foregroundColor: UIColor.label,
-            NSAttributedString.Key.font : UIFont.systemFont(ofSize: 12),
-//            preferredFont(forTextStyle: UIFont.TextStyle.title1),
-            NSAttributedString.Key.paragraphStyle : style
-        ]
-        
-        let attributedString = NSMutableAttributedString(string: title, attributes: titleAttributes)
-        
-        setAttributedTitle(attributedString, for: UIControl.State.normal)
-        titleLabel?.numberOfLines = 0
-        titleLabel?.lineBreakMode = NSLineBreakMode.byWordWrapping
-//        titleLabel?.font = UIFont(name: "Helvetica", size: 14)
+        titleLabel?.font = UIFont(name: "Helvetica", size: 14)
     }
     
     required init?(coder aDecoder: NSCoder) {
